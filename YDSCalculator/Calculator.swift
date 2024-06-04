ae61ce02abd4763cb88c5848479d88d5c221451d
daesung <eogud8203@naver.com>
2024-06-04 22:28:14 +0900

    Lv01 first push

---
+++ YDSCalculator/Calculator.swift
@@ -0,0 +1,47 @@
+//
+//  Calculator.swift
+//  YDSCalculator
+//
+//  Created by 머성이 on 6/4/24.
+//
+
+import Foundation
+
+
+class Calculator{
+    func calculate(oper: String, firstNumer: Int, secondNumber: Int ) -> Int {
+        switch oper {
+        case "+":
+            return firstNumer + secondNumber
+        case "-":
+            return firstNumer - secondNumber
+        case "*":
+            return firstNumer * secondNumber
+        case "/":
+            return firstNumer / secondNumber
+        default:
+            print("옳바른 값을 입력하세요.")
+        }
+        return 0
+    }
+    
+    // 숫자 입력 받기
+    func inputA(prompt: String) -> Int {
+        print(prompt,terminator:"")
+        guard let input = readLine(), let number = Int(input) else {
+            print("유효한 숫자를 입력해주세요.")
+            return inputA(prompt: prompt)
+        }
+        return number
+    }
+
+    // 연산자 입력 받기
+    func getoper(prompt: String) -> String {
+        print(prompt,terminator: "")
+        guard let operation = readLine(), ["+", "-", "*", "/"].contains(operation) else {
+            print("유효한 연산자를 입력해주세요 (+, -, *, /).")
+            return getoper(prompt: prompt)
+        }
+        return operation
+    }
+}
--- YDSCalculator/main.swift
+++ YDSCalculator/main.swift
@@ -1,11 +1,14 @@
-//
-//  main.swift
-//  YDSCalculator
-//
-//  Created by 머성이 on 6/4/24.
-//
 
 import Foundation
 
-print("Hello, World!")
+let car = Calculator()
 
+// 사용자로부터 입력 받기
+let firstNumber = car.inputA(prompt: "첫 번째 숫자를 입력하세요:")
+let operation = car.getoper(prompt: "연산자를 입력하세요 (+, -, *, /):")
+let secondNumber = car.inputA(prompt: "두 번째 숫자를 입력하세요:")
+
+// 계산 및 결과 출력
+let result = car.calculate(oper: operation, firstNumer: firstNumber, secondNumber: secondNumber)
+
+print(result)
